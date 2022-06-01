32fa19e4677e934d0a580fc79c28df741ab23b7a
Bas Spijkers <DfeTheBas@hotmail.com>
2022-06-01 17:01:28 +0200

    Update funnies.py

--- funnies.py
+++ funnies.py
@@ -35,7 +35,8 @@ def kill(author, victim):
         'https://c.tenor.com/YNK5CjT9dw8AAAAC/kill-wasted.gif',
         'https://c.tenor.com/mZ1h2IELTmoAAAAC/wasted-shovel.gif',
         'https://c.tenor.com/6BEtB9KcY2YAAAAC/funny-kid.gif',
-        'https://tenor.com/view/murderer-pillow-gif-25218991'
+        'https://c.tenor.com/t25gIlKm8UEAAAAC/murderer-pillow.gif',
+        'https://c.tenor.com/9jf-DDWOCI4AAAAC/runover-kid.gif'
     ]
 
     suicide_gifs = [
